8b592a9889cfbd279c1cdf8c93c17849681c7f3c
thanh <vuhoangthanh0710@gmail.com>
2025-01-02 21:23:12 +0700

    set Output exception 'Token'

--- src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/vn/hoidanit/jobhunter/config/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.jobhunter.config;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
@@ -34,7 +35,12 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
         RestResponse<Object> res = new RestResponse<Object>();
         res.setStatusCode(HttpStatus.UNAUTHORIZED.value());
-        res.setError(authException.getCause().getMessage());
+
+        String errorMessage = Optional.ofNullable(authException.getCause())
+                .map(Throwable::getMessage)
+                .orElse(authException.getMessage());
+
+        res.setError(errorMessage);
         res.setMessage("Token không hợp lệ (hết hạn, không đúng định dạng, hoặc...)");
 
         mapper.writeValue(response.getWriter(), res);
